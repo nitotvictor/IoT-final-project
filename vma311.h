5c19f8504f37f73444cd92c547c504dfbc0ca0e5
nitotvictor <75942415+nitotvictor@users.noreply.github.com>
2021-03-29 22:28:26 +0200

    Update vma311.h

--- vma311.h
+++ vma311.h
@@ -3,7 +3,7 @@
 
 #include "driver/gpio.h"
 
-/* type definitions */
+// type definitions
 typedef enum vma311_status
 {
     VMA311_CRC_ERROR = -2,
@@ -11,24 +11,24 @@ typedef enum vma311_status
     VMA311_OK
 } vma311_status_t;
 
-typedef struct vma311_data
+typedef struct vma311_data //Defining the type of variables (int and decimals) with int8_t
 {
     vma311_status_t status;
-    int8_t rh_int;
-    int8_t rh_dec;
-    int8_t t_int;
-    int8_t t_dec;
+    int8_t rh_int; //int humidity
+    int8_t rh_dec; //decimal humidity
+    int8_t t_int; //int temp
+    int8_t t_dec; //decimal temp
 } vma311_data_t;
 
-typedef struct vma311
+typedef struct vma311 //to define which gpio is linked and collect the values 
 {
     gpio_num_t num;
     int64_t last_read_time;
     vma311_data_t data;
 } vma311_t;
 
-/* function prototypes */
-void          vma311_init(gpio_num_t);
-vma311_data_t vma311_get_values();
+//function prototypes 
+void          vma311_init(gpio_num_t); //to set the gpio linked
+vma311_data_t vma311_get_values(); //get values that we will use in the main function
 
-#endif /* __VMA311_H__ */
+#endif
