d135f8c6143e0776ab1f5c1b9125886bf61d02c0
nitotvictor <75942415+nitotvictor@users.noreply.github.com>
2021-03-30 22:25:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h" //sets configuration required to run freeRTOS on ESP32
+#include "freertos/task.h" //provides the multitasking functionality
+#include "sdkconfig.h" //make sdkconfig options available to the project build system and source files
 #include "led.h"
 #include "mcp9700.h"
 #include "vma311.h"
@@ -11,20 +11,16 @@
 #include "mqtt.h"
 #include "bme680.h"
 
-/**
- * These macros define characteristics of devices
- */
-#define BUILTIN_LED_GPIO GPIO_NUM_2
-#define MCP9700_ADC_UNIT ADC_UNIT_1
-#define MCP9700_ADC_CHANNEL ADC_CHANNEL_4
-#define VMA311_GPIO GPIO_NUM_5
 
-/**
- * This function is the main function of the application.
- */
+#define BUILTIN_LED_GPIO GPIO_NUM_2 //GPIO 2 assigned to LED
+#define MCP9700_ADC_UNIT ADC_UNIT_1 //ADC1 for MCP9700
+#define MCP9700_ADC_CHANNEL ADC_CHANNEL_4 //Channel 4 for MCP9700
+#define VMA311_GPIO GPIO_NUM_5 //GPIO 5 assigned to VMA311
+
 
 void app_main()
 {
+    //Create variables for each sensor
     uint32_t mcp_temp;
     char mcp9700_temperature[8];
     char vma311_temperature[8];
@@ -41,18 +37,18 @@ void app_main()
     
 
     /* Device initialization */
-    wifi_init("Freebox-A28900", "condida-sospitatis6-gemellorum-emoti8");
+    wifi_init("Freebox-A28900", "condida-sospitatis6-gemellorum-emoti8"); //wifi connection
     //wifi_init("Raspberry", "esilv-evd21");
     //wifi_init("iPhone", "azertyazerty");
-    aio_init("victornitot","aio_wSii70UyFJTrweGsyyK4X33loIpq");
+    aio_init("victornitot","aio_wSii70UyFJTrweGsyyK4X33loIpq"); //adafruit
     aio_create_group("envmon");
     aio_create_feed("mcp9700","envmon");
     aio_create_feed("vma311","envmon");
-    mcp9700_init(MCP9700_ADC_UNIT, MCP9700_ADC_CHANNEL);
-    mqtt_init("mqtts://@iot.devinci.online", "vn170735", "%%@s5$ZQ");
-    vma311_init(VMA311_GPIO);
+    mcp9700_init(MCP9700_ADC_UNIT, MCP9700_ADC_CHANNEL); //mcp9700 init
+    mqtt_init("mqtts://@iot.devinci.online", "vn170735", "%%@s5$ZQ"); //mqtt
+    vma311_init(VMA311_GPIO); //vma311 init
     bme.intf = BME680_I2C_INTF;
-    bme680_init(&bme);
+    bme680_init(&bme); //bme680 init
 
     
 
@@ -72,7 +68,7 @@ void app_main()
         vma311_data = vma311_get_values();
         sprintf(vma311_temperature,"%d", vma311_data.t_int);
         sprintf(vma311_humidity,"%d", vma311_data.rh_int);
-        if (vma311_data.status == VMA311_OK)
+        if (vma311_data.status == VMA311_OK) //if no time out error absurdity 
         {
             printf("vma311:temp:%d.%d\n", vma311_data.t_int, vma311_data.t_dec);
             printf("vma311:humidity:%d.%d\n", vma311_data.rh_int, vma311_data.rh_dec);
@@ -93,7 +89,7 @@ void app_main()
         printf("bme680:humidity:%d\n", bme_data.humidity);
         printf("bme680:pressure:%d\n", bme_data.pressure);
         printf("bme680:gas_resistance:%d\n", bme_data.gas_resistance);
-        sprintf(bme680_temperature,"%d", bme_data.temperature);
+        sprintf(bme680_temperature,"%d", bme_data.temperature); //Convert the int into a char
         sprintf(bme680_humidity,"%d", bme_data.humidity);
         sprintf(bme680_pressure,"%d", bme_data.pressure);
         sprintf(bme680_gas_resistance,"%d", bme_data.gas_resistance);
@@ -109,6 +105,6 @@ void app_main()
         mqtt_publish("vn170735/bme680/gas_resistance",bme680_gas_resistance);
         
      
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(5000 / portTICK_PERIOD_MS); // 5 sec delay
     }
 }
